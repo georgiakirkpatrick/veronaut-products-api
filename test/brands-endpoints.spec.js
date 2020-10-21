7f4435dc43e2bf1abe0191b9db4d5e7b216bc9b6
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2020-10-21 16:37:58 -0700

    Update db to database for heroku deploy

--- test/brands-endpoints.spec.js
+++ test/brands-endpoints.spec.js
@@ -46,7 +46,7 @@ describe('Brands Endpoints', function() {
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL,
+            connection: process.env.TEST_DATABASE_URL,
         })
         app.set('db', db)
     })
