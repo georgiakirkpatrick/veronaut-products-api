a3a4df26b029a94634acebf652d5d28444df4cf8
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2021-04-19 14:32:02 -0700

    Debug heroku fix

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
-const connection = (NODE_ENV !== 'production' ? DATABASE_URL :
+const connection = (process.env.NODE_ENV !== 'production' ? DATABASE_URL :
   { 
       connectionString: DATABASE_URL,
       ssl: { rejectUnauthorized: false }
